b870880a4af8f8a5ddef5291dc40a75879c7af12
Nathan Bloom <nabloomer@gmail.com>
2015-06-10 11:52:25 -0500

    White listed params to pass photo to the views.

--- hiprest_app/app/controllers/restaurants_controller.rb
+++ hiprest_app/app/controllers/restaurants_controller.rb
@@ -70,6 +70,6 @@ class RestaurantsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :vote)
+      params.require(:restaurant).permit(:name, :address, :vote, :photo)
     end
 end
