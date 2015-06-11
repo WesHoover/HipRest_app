cfbb02a05977fa0259081b3fd21258527993e865
Nathan Bloom <nabloomer@gmail.com>
2015-06-11 00:27:39 -0500

    Debug foriegn key populatable in restaurants table

--- hiprest_app/app/models/user.rb
+++ hiprest_app/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-	has_many :reviews
-	has_many :restaurants
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  	devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :trackable, :validatable
+	has_many :restaurants
+	has_many :reviews
 end
